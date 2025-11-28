4e372789eeacb68a7dec9a09b9329dda400133a6
REICHHART Damien <damien.reichhart@gmail.com>
2025-11-28 20:01:25 +0100

    Added _ for unused func n error handler

--- apps/api/src/middlewares/error-handler.ts
+++ apps/api/src/middlewares/error-handler.ts
@@ -1,7 +1,7 @@
 import { logger } from "../libs/pino";
 import type { Request, Response, NextFunction } from "express";
 
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+export function errorHandler(err: Error, req: Request, res: Response, _next: NextFunction) {
   logger.error({ err, req }, "unhandled error");
 
   res.status(500).json({
